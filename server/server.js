d6fa4a7071b00e812f29ab36c1527ffc40fdc814
fiftyfivebells <stephen.l.bell@gmail.com>
2017-10-12 20:30:16 -0400

    Update port

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 
 app.use('/', routes);
 
-const port = process.env.Port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
