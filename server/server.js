88f3c0dc9cddea2bf0bcff4e30ca61363b7551ee
fiftyfivebells <stephen.l.bell@gmail.com>
2017-10-13 20:31:18 -0400

    Add mongoose db and update routes to polls/accounts

--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const routes = require('./routes/routes.js');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const pollRoutes = require('./routes/polls.js');
+const acctRoutes = require('./routes/accounts.js');
+
+// connect to mongo through mongoose
+mongoose.connect('mongodb://127.0.0.1');
 
 // create static asses from the front-end's bundle
 const staticFile = express.static(path.join(__dirname, '../client/dist'));
@@ -9,6 +15,7 @@ const staticFile = express.static(path.join(__dirname, '../client/dist'));
 app.use(staticFile);
 app.set('trust proxy', true);
 
+app.use(bodyParser.json());
 app.use((req, res, next) => {
     const headers = 'Access-Control-Allow-Headers';
     res.header('Access-Control-Allow-Origin', '*');
@@ -16,7 +23,14 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use('/', routes);
+// handle the different routes
+app.use('/polls', pollRoutes);
+app.use('/account', acctRoutes);
+
+// for all other routes, use Vue client
+app.get('*', (req, res) => {
+    res.send(staticFile);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
