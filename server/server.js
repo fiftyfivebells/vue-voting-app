8394efbc334631694631600ce7eafa19d01786d6
fiftyfivebells <stephen.l.bell@gmail.com>
2017-10-16 20:10:30 -0400

    Finish server setup

--- server/server.js
+++ server/server.js
@@ -7,19 +7,30 @@ const pollRoutes = require('./routes/polls.js');
 const acctRoutes = require('./routes/accounts.js');
 
 // connect to mongo through mongoose
+mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://127.0.0.1');
 
 // create static asses from the front-end's bundle
 const staticFile = express.static(path.join(__dirname, '../client/dist'));
 
 app.use(staticFile);
+app.use(express.static('testpublic'));
 app.set('trust proxy', true);
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+// enable CORS
 app.use((req, res, next) => {
-    const headers = 'Access-Control-Allow-Headers';
+    const headers = {
+        methods: 'PUT, GET, POST, DELETE, OPTIONS',
+        headers: 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials',
+    };
+
     res.header('Access-Control-Allow-Origin', '*');
-    res.header(headers, 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', headers.methods);
+    res.header('Access-Control-Allow-Headers', headers.headers);
+    res.header('Access-Control-Allow-Credentials', true);
     next();
 });
 
@@ -27,8 +38,8 @@ app.use((req, res, next) => {
 app.use('/polls', pollRoutes);
 app.use('/account', acctRoutes);
 
-// for all other routes, use Vue client
-app.get('*', (req, res) => {
+// redirect any non-specific routes to the front-end client
+app.use('/*', (req, res) => {
     res.send(staticFile);
 });
 
