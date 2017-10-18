7bffc89f5887e0c68c79711ba0c307baf3cd9612
fiftyfivebells <stephen.l.bell@gmail.com>
2017-10-17 20:52:35 -0400

    Add functionality to post routes for accounts

--- server/routes/accounts.js
+++ server/routes/accounts.js
@@ -1,8 +1,67 @@
 const express = require('express');
+const JWT = require('jsonwebtoken');
 const router = express.Router();
-const path = require('path');
-
 const passport = require('passport');
 const Account = require('../models/account');
 
-passport.use(new LocalStrategy(Account.authenticate()));
+/**
+ * Creates a JWT token to identify user
+ * @param {*} user
+ * @return {obj} A token attached to user to authenticate their activities 
+ */
+function generateToken(user) {
+    const jwtOptions = {
+        name: user._id,
+        time: 10080,
+    };
+    return JWT.sign(jwtOptions, process.env.JWT_KEY || 'prokchop');
+}
+
+router.route('/register')
+    .post(async (req, res, next) => {
+        const {username, password} = req.body;
+
+        console.log(password);
+
+        if (!username) {
+            return res.status(422).send({error: 'Please enter username'});
+        }
+
+        if (!password) {
+            return res.status(422).send({error: 'Please enter password'});
+        }
+
+        const newAcc = new Account({username, password});
+        const acct = await newAcc.save()
+        .catch((err) => { 
+            return {err: 'user already exists'};
+        });
+
+        const user = {
+            name: username,
+            id: acct._id,
+        };
+
+        console.log(user);
+        const webToken = generateToken(user);
+
+        res.set('Authorization', webToken).json(acct);
+    });
+
+router.route('/login')
+    .post(async (req, res, next) => {
+        passport.authenticate('local', {session: false}, (err, user, info) => {
+            if (err) {
+                return next(err);
+            };
+            if (!user) {
+                return res.json( {message: info.message});
+            }
+
+            const webToken = generateToken(user);
+            res.set('Authorization', webToken).json(user);
+            })(req, res, next);
+    });
+
+module.exports = router;
+
