286735cceaa6b9f8a50fff968f84486baacade45
fiftyfivebells <stephen.l.bell@gmail.com>
2017-10-21 12:30:38 -0400

    Fix scope issue with jwt options object

--- server/passport/passport.js
+++ server/passport/passport.js
@@ -25,13 +25,12 @@ const local = new LocalStrategy(
             });
     });
 
+const jwtOptions = {
+    jwtFromRequest: extractJwt.fromHeader(),
+    secretOrKey: process.env.JWT_KEY || 'justfortesting',
+};
+
 const jwt = new JwtStrategy(jwtOptions, async (data, next) => {
-
-    const jwtOptions = {
-        jwtFromRequest: extractJwt.fromHeader(),
-        secretOrKey: process.env.JWT_KEY || 'justfortesting',
-    };
-
     const acc = await Account.findById(data._id);
     if (acc) {
         return next(null, acc);
