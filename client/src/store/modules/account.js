0397d8ec33767ae22cf678bbba1c9a069a6441cf
fiftyfivebells <stephen.l.bell@gmail.com>
2017-10-21 12:32:13 -0400

    Add account module

---
+++ client/src/store/modules/account.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+const server = axios.create('localhost:3000');
+
+export default {
+    state: {
+        username: '',
+        _id: '',
+        token: '',
+    },
+    mutations: {
+        createUser: (state, data) => {
+            state.username = data.username;
+            state.id = data._id;
+        },
+        addToken: (state, data) => {
+            state.token = data;
+        },
+    },
+    actions: {
+        logIn: async ({commit}, data) => {
+            const obj = await server.post('/account/login', data);
+            console.log(obj);
+        },
+        register: async ({commit}, data) => {
+            const obj = await server.post('/account/register', data);
+            console.log(obj);
+        },
+    },
+    getters: {
+        username: (state) => state.username,
+        id: (state) => state._id,
+        token: (state) => state.token,
+        isAuth: (state) => !!state.token,
+    },
+};
