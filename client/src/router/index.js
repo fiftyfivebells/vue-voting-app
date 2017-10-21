d99754c5feee6788dca5312f841505bf1f0d115b
fiftyfivebells <stephen.l.bell@gmail.com>
2017-10-21 12:31:23 -0400

    Update router with new routes

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,6 +3,9 @@ import Router from 'vue-router';
 
 // routes to different views
 import Home from '../components/Home.vue';
+import Login from '../components/Login.vue';
+import Polls from '../components/Polls.vue';
+import Register from '../components/Register.vue';
 
 Vue.use(Router);
 
@@ -10,6 +13,15 @@ export default new Router({
     routes: [
         {
             path: '/', component: Home,
+        },
+        {
+            path: '/login', component: Login,
+        },
+        {
+            path: '/register', component: Register,
+        },
+        {
+            path: '/polls', component: Polls,
         },
     ],
 });
