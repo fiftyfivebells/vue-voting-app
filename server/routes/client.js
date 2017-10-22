00f0d09844f0279e01f7232b70e2e50bd61ecdbc
fiftyfivebells <stephen.l.bell@gmail.com>
2017-10-22 18:44:36 -0400

    Require path for client routes

--- server/routes/client.js
+++ server/routes/client.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 // create static asses from the front-end's bundle
 const staticFile = express.static(path.join(__dirname, '../../client/dist'));
