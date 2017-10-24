493cf1ba916afe96f836d3daf6d873d444fb5799
fiftyfivebells <stephen.l.bell@gmail.com>
2017-10-24 12:46:11 -0400

    Change join to resolve

--- server/routes/client.js
+++ server/routes/client.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const path = require('path');
 
 // create static asses from the front-end's bundle
-const staticFile = express.static(path.join(__dirname, '../../client/dist/index.html'));
+const staticFile = express.static(path.resolve(__dirname, '../../client/dist/index.html'));
 
 router.get('/*', (req, res) => {
     res.sendFile(staticFile);
