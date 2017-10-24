6226c511927b9b4fb54e9f149302fd9d718f2ba0
fiftyfivebells <stephen.l.bell@gmail.com>
2017-10-24 12:31:27 -0400

    Send client build's HTML file

--- server/routes/client.js
+++ server/routes/client.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const path = require('path');
 
 // create static asses from the front-end's bundle
-const staticFile = express.static(path.join(__dirname, '../../client/dist'));
+const staticFile = express.static(path.join(__dirname, '../../client/dist/index.html'));
 
 router.get('/*', (req, res) => {
-    res.send(staticFile);
+    res.sendFile(staticFile);
 });
 
 module.exports = router;
