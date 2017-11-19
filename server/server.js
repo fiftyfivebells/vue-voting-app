be6dd2486305b7cd41024f8cf383bc2e925bc292
fiftyfivebells <stephen.l.bell@gmail.com>
2017-11-18 20:13:04 -0500

    Require babel-polyfill

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
+require('babel-polyfill');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
