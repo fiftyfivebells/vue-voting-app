192c0e87439f5a9d8b42132325104ac2a029d8d6
fiftyfivebells <stephen.l.bell@gmail.com>
2017-11-19 12:20:13 -0500

    Create poll module in store

--- client/src/store/modules/poll.js
+++ client/src/store/modules/poll.js
@@ -1,3 +1,26 @@
-export default {
+import axios from 'axios';
+const server = axios.create('localhost:3000');
 
+export default {
+    state: {
+        polls: [],
+    },
+    mutations: {
+        addNewPoll: (state, data) => {
+            state.polls.push(data);
+        },
+    },
+    actions: {
+        addPoll: async ({commit}, data) => {
+            try {
+                const obj = await server.post('http://localhost:3000/polls/add', data);
+                console.log(obj);
+            } catch (err) {
+                console.log(err);
+            }
+        },
+    },
+    getters: {
+        polls: (state) => state.polls,
+    },
 };
