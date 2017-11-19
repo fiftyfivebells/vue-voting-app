d517c46a0f59c9a63216693263343bf41a5631bb
fiftyfivebells <stephen.l.bell@gmail.com>
2017-11-19 12:22:29 -0500

    Create component for adding polls

---
+++ client/src/components/addPoll.vue
@@ -0,0 +1,112 @@
+<template>
+    <div>
+        <v-layout column>
+            <v-flex xs6 offset-xs3>
+                <v-card>
+                <v-card-title>
+                    <v-flex>
+                        <h3>Add a Poll</h3>
+                    </v-flex>
+                </v-card-title>
+                <v-card-text>
+                    <v-container>
+                        <v-form>
+                            <v-layout>
+                                <v-flex>
+                                    <h4>Enter a poll question:</h4>
+                                    <v-text-field
+                                    v-model='question'
+                                    name='question'
+                                    type='text'
+                                    label='Enter question here'
+                                    required>
+                                    </v-text-field>
+                                </v-flex>
+                            </v-layout>
+                            <v-layout column>
+                                <v-flex>
+                                    <h4>Enter the choices</h4>
+                                    <v-text-field
+                                    v-model='choices[0]'
+                                    type='text'
+                                    label='Choice 1'
+                                    required>
+                                    </v-text-field>
+                                    <v-text-field
+                                    v-model='choices[1]'
+                                    type='text'
+                                    label='Choice 2'
+                                    required>
+                                    </v-text-field>
+                                </v-flex>
+                                <v-flex v-for='(extraChoice, idx) in moreChoices' :key='idx'>
+                                    <v-text-field
+                                    v-model='moreChoices[idx]'
+                                    type='text'
+                                    label='Bonus option'>                                        
+                                    </v-text-field>
+                                </v-flex>
+                                <v-flex>
+                                    <v-text-field
+                                    v-model='choice'
+                                    type='text'
+                                    label='Bonus option'>                                        
+                                    </v-text-field>
+                                    <v-btn @click='addOption'>Add Bonus Option</v-btn>
+                                    <v-btn @click='deleteOptions'>Delete Bonus Options</v-btn>
+                                </v-flex> 
+                                <v-flex>
+                                    <v-btn>Submit Poll</v-btn>
+                                </v-flex>                                   
+                            </v-layout>
+                        </v-form>
+                        <div>
+                            <p>{{ question }}</p>
+                            <p>{{ choices }}</p>
+                            <p>{{ choice }}</p>
+                            <p>{{ moreChoices }}</p>
+                        </div>
+                    </v-container>
+                </v-card-text>
+            </v-card>
+            </v-flex>                
+        </v-layout>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            question: '',
+            choices: [],
+            choice: '',
+            moreChoices: [],
+            author: this.$store.state.user,
+        }
+    },
+    methods: {
+        async addPoll() {
+            try {
+                await this.$store.dispatch('addPoll', {
+                    question: this.question,
+                    author: this.author,
+                    choices: [...this.choices, ...this.moreChoices],
+                });
+                this.$router.push('/');
+            } catch (err) {
+                console.log(err);
+            }
+        },
+        addOption() {
+            this.moreChoices.push(this.choice);
+            this.choice = '';
+        },
+        deleteOptions() {
+            this.moreChoices = [];
+            this.choice = '';
+        }
+    }
+}
+</script>
+
