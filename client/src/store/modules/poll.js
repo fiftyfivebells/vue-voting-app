881ffb64d2e5d83ad0aa15770eb8a763d312dae6
fiftyfivebells <stephen.l.bell@gmail.com>
2017-12-11 08:29:16 -0500

    Finish functionality for getAllPolls and addPoll

--- client/src/store/modules/poll.js
+++ client/src/store/modules/poll.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const server = axios.create('localhost:3000');
+const server = 'https://ffb-voting-app.herokuapp.com/api/polls';
+
 
 export default {
     state: {
@@ -9,16 +10,28 @@ export default {
         addNewPoll: (state, data) => {
             state.polls.push(data);
         },
+        getPolls: (state, data) => {
+            state.polls = data;
+        }
     },
     actions: {
         addPoll: async ({commit}, data) => {
             try {
-                const obj = await server.post('http://localhost:3000/polls/add', data);
+                const obj = await axios.post(server + '/add', data);
                 console.log(obj);
             } catch (err) {
                 console.log(err);
             }
         },
+        getAllPolls: async ({commit}, data) => {
+            try {
+                const polls = await axios.get(server + '/', data);
+                console.log(polls);
+                commit('getPolls', polls.data);
+            } catch (err) {
+                console.log(err);
+            }
+        }
     },
     getters: {
         polls: (state) => state.polls,
