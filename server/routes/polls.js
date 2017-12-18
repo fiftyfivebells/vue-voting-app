cb6c83cfe5890bd4a4b100aa69f87e351d88e18f
fiftyfivebells <stephen.l.bell@gmail.com>
2017-12-17 19:32:53 -0500

    Fix typo

--- server/routes/polls.js
+++ server/routes/polls.js
@@ -23,7 +23,7 @@ router.route('my-polls')
 
 router.route('/delete')
     .post(async (req, res) => {
-        await Poll.delete({choice: req.body.question});
+        await Poll.deleteOne({choice: req.body.question});
         res.json('poll deleted');
     });
 
