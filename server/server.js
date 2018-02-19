e13f1697b433009e39f7cc6d2869572391f334e4
fiftyfivebells <stephen.l.bell@gmail.com>
2018-02-18 19:37:29 -0500

    Attempt to enable cors with the cors middleware

--- server/server.js
+++ server/server.js
@@ -18,12 +18,12 @@ mongoose.connect(mongoUrl);
 
 // enable CORS
 app.use(cors());
-app.all('*', cors({
+/* app.all('*', cors({
     origin: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     exposedHeaders: ['Authorization'],
-}));
+})); */
 /* app.all('*', (req, res, next) => {
     const headers = {
         methods: 'PUT, GET, POST, DELETE',
